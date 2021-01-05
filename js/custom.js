0ab660421b5bcb3991d35831281ea7a1663da88c
jahanzaibmaqsood <jahanzaibmaqsood@gmail.com>
2021-01-05 18:00:45 +0500

    design 3

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,46 @@
-// window.addEventListener('scroll', runOnScroll);
-// let navBar = document.getElementById('navBar');
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// function runOnScroll(){
-//     if(pageYOffset >= 1){
-    
-//         navBar.classList.add('navbar-light');
-//         navBar.classList.add('bg-light');
-//         navBar.classList.remove('navbar-dark');
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-//     } else if(pageYOffset <= 1)
-//     {
-    
-//         navBar.classList.remove('navbar-light');
-//         navBar.classList.remove('bg-light');
-//         navBar.classList.add('navbar-dark');
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-//     }
-// }
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+$(document).ready(function() {
+  $('.minus').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  $('.plus').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+  });
+});
